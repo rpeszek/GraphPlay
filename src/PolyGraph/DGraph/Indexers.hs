1172a7f8f46c4df3051c2134c2b0ce9a4e576ee6
rpeszek <peszeks@gmail.com>
2016-07-04 11:46:22 -0600

    fast dedges

--- src/PolyGraph/DGraph/Indexers.hs
+++ src/PolyGraph/DGraph/Indexers.hs
@@ -1,5 +1,9 @@
 
-module PolyGraph.DGraph.Indexers where
+module PolyGraph.DGraph.Indexers (
+   BuildableCollection
+   , buildHmCIndex
+   , CIndexContainer -- snould not be needed
+) where
 
 import qualified Data.Maybe as MB
 import Control.Monad
@@ -9,40 +13,47 @@ import qualified Data.HashMap.Strict as HM
 import PolyGraph.Helpers
 import PolyGraph.DGraph
 
+-----------------------------------------------------------------------
+-- builders that create fast CIndex implemenations for any DGraph   ---
+-----------------------------------------------------------------------
+
 data CIndexContainer v e t = CIndexContainer {
     indexedcEdgesOf :: v -> t e
 }
+
+instance forall v e t . (Hashable v, Eq v, DEdgeSemantics e v, Traversable t) => CIndex (CIndexContainer v e t) v e t where
+   cEdgesOf = indexedcEdgesOf
 
 class (Eq k) => BuildableMap i k v | i -> k, i-> v where
    safeKeyOverValue ::  i -> k -> (v -> v) -> v ->  i
    emptyMap ::  i
    safeLookup ::  i -> k -> v -> v
 
-class (Traversable t) => BuildableTraversable t  where
-   addElement :: e -> t e -> t e
+class (Foldable t) => BuildableCollection t  where
+   prependElement  :: e -> t e -> t e
    emptyCollection :: t e
 
-buildMap :: forall g v e t0 t i. (DGraph g v e t0, BuildableTraversable t, BuildableMap i v (t e)) => g -> i
+buildMap :: forall g v e t0 t i. (DGraph g v e t0, BuildableCollection t, Traversable t, BuildableMap i v (t e)) => g -> i
 buildMap g =
   let gedges = edges g  :: t0 e
       i0  = emptyMap    :: i
   in
-      foldr (\e i -> safeKeyOverValue i (first' . resolveVertices $ e) (addElement e) emptyCollection) i0 gedges
+      foldr (\e i -> safeKeyOverValue i (first' . resolveVertices $ e) (prependElement e) emptyCollection) i0 gedges
 
 --
 -- specialize implementation using HashMap
 --
 
-buildHM :: forall g v e t0 t i. (Eq v, Hashable v, DGraph g v e t0, BuildableTraversable t) => g -> HM.HashMap v (t e)
+buildHM :: forall g v e t0 t i. (Eq v, Hashable v, DGraph g v e t0, BuildableCollection t, Traversable t) => g -> HM.HashMap v (t e)
 buildHM = buildMap
 
-buildHmIndex :: forall g v e t0 t. (Hashable v, Eq v, DGraph g v e t0, BuildableTraversable t) => g -> CIndexContainer v e t
-buildHmIndex g =
+buildHmCIndex :: forall g v e t0 t. (Hashable v, Eq v, DGraph g v e t0, BuildableCollection t, Traversable t) => g -> CIndexContainer v e t
+buildHmCIndex g =
     let hm = buildHM g :: HM.HashMap v (t e)
         cEdgesOfImpl = (\v -> HM.lookupDefault emptyCollection v hm) :: v -> t e
     in CIndexContainer { indexedcEdgesOf = cEdgesOfImpl}
 
---- INSTANCES ---
+--- Helper instances For CIndexing work ---
 
 instance forall k v. (Hashable k, Eq k) => BuildableMap (HM.HashMap k v) k v where
   safeKeyOverValue hm k f defaultV =
@@ -52,23 +63,45 @@ instance forall k v. (Hashable k, Eq k) => BuildableMap (HM.HashMap k v) k v whe
   emptyMap = HM.empty
   safeLookup hm k v = HM.lookupDefault v k hm
 
-instance  BuildableTraversable [] where
-  addElement = (:)
+instance  BuildableCollection [] where
+  prependElement = (:)
   emptyCollection = []
 
-instance forall v e t . (Hashable v, Eq v, DEdgeSemantics e v, Traversable t) => CIndex (CIndexContainer v e t) v e t where
-   cEdgesOf = indexedcEdgesOf
+
+-----------------------------------------------------------------------------------------
+--  DEdgeSemantics indexer should be needed only if rerieval of v-s from edges is slow --
+-----------------------------------------------------------------------------------------
+
+data DEdgeWithFastSemantics e v = DEdgeWithIndexedSemantics {
+    getDEdge  :: e,
+    getVertices   :: (v,v)
+}
+
+
+instance forall v e map. DEdgeSemantics (DEdgeWithFastSemantics e v) v where
+   resolveVertices indexedE = getVertices indexedE
+
+emptyFastDEgdes :: forall t e v . (BuildableCollection t) =>  t (DEdgeWithFastSemantics e v)
+emptyFastDEgdes = emptyCollection
+
+buidFastDEdges :: forall t e v t0. (Foldable t, BuildableCollection t0) =>
+                             t0 (DEdgeWithFastSemantics e v) ->  (e -> (v,v)) -> t e -> t0 (DEdgeWithFastSemantics e v)
+buidFastDEdges _emptyCollection _slowResolveVertices _slowEdgeCollection =
+        --foldr :: (a -> b -> b) -> b -> t a -> b
+        foldr (\oldEdge inxEdges ->
+                  let newEdge = DEdgeWithIndexedSemantics {getDEdge = oldEdge, getVertices = _slowResolveVertices oldEdge}
+                  in prependElement newEdge inxEdges) _emptyCollection _slowEdgeCollection
 
 ----------------------------
 -- IDEAS that did not work:
 ----------------------------
 
 --currently not used
-class Benign m where
-  runBenign :: (forall s . m s a) -> a
+--class Benign m where
+--  runBenign :: (forall s . m s a) -> a
 
-instance  Benign ST where
-    runBenign = runST
+--instance  Benign ST where
+--    runBenign = runST
 
 --removing ST s from paritally applied HashTable s v [e] is no allowed by typechecker. s-s do not much
 
