f54f50a0e4a74a54a4632b0563e1b31675b28466
rpeszek <peszeks@gmail.com>
2016-07-14 20:30:29 -0600

    Poly Morthism

--- examples/Play/DiGraph/PolyRebuild.hs
+++ examples/Play/DiGraph/PolyRebuild.hs
@@ -5,6 +5,7 @@ import PolyGraph.Buildable.PolyRebuild
 import PolyGraph.Buildable.PolyMorth
 import qualified PolyGraph.Instances.SimpleGraph as SG
 import qualified PolyGraph.Instances.DiGraph.HashMapAsDiGraph as HM
+import Data.Hashable
 
 -- create simple graph data structure
 edgeList :: [(Int, Int)]
@@ -18,12 +19,18 @@ diamond0123Simple = SG.SimpleGraph edgeList []
 diamond0123 :: forall g t . (BuildableGraphDataSet g Int (Int, Int) t) =>  g
 diamond0123 = polyRebuild diamond0123Simple
 
+diamond3456 :: forall g t . (BuildableGraphDataSet g Int (Int, Int) t) =>  g
+diamond3456 = pmorth (+3) diamond0123Simple
+
+diamondChain :: forall g t . (BuildableGraphDataSet g Int (Int, Int) t) =>  g
+diamondChain =  foldr (\i g -> g `union` (pmorth (+i) diamond0123Simple)) empty (map (*3) [0..5])
 
 -- specialized consumption
 -- use it as something else
 
-{-
+
 -- TODO does not work, why?
+{-
 showAsHashMapHelper :: forall g t . (BuildableGraphDataSet g Int (Int, Int) t) => g -> String
 showAsHashMapHelper gr =
                         let mygraph :: HM.DiGraphHashMap Int (Int,Int) []
@@ -37,15 +44,11 @@ showDiamond0123AsHashMap = let mygraph :: HM.DiGraphHashMap Int (Int,Int) []
 
 --showDiamond0123AsHashMap = showAsHashMapHelper diamond0123
 
-diamond3456 :: forall g t . (BuildableGraphDataSet g Int (Int, Int) t) =>  g
-diamond3456 = pmorth (+3) diamond0123Simple
 
 showDiamond3456AsHashMap = let mygraph :: HM.DiGraphHashMap Int (Int,Int) []
                                mygraph = diamond3456
                            in  show (mygraph)
 
-diamondChain :: forall g t . (BuildableGraphDataSet g Int (Int, Int) t) =>  g
-diamondChain =  foldr (\i g -> g `union` (pmorth (+i) diamond0123Simple)) empty (map (*3) [0..5])
 
 showDiamondChainAsHashMap  = let mygraph :: HM.DiGraphHashMap Int (Int,Int) []
                                  mygraph = diamondChain
