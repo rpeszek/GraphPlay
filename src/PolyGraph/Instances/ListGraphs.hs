b3d048c1e3d9836071aecab5dfa988f1ee94e534
rpeszek <peszeks@gmail.com>
2016-07-15 21:01:09 -0600

    ListGraphs work

--- src/PolyGraph/Instances/ListGraphs.hs
+++ src/PolyGraph/Instances/ListGraphs.hs
@@ -16,13 +16,13 @@ import PolyGraph.Common.Helpers
 import PolyGraph.Common.BuildableCollection
 import Data.List (nub, null, lines, words, concat)
 
--- | this graph has no edges
+-- | this graph has no edges, it acts in forgetful way if edges are added
 newtype Vertices v e = Vertices { getVertices :: [v]} deriving Show
 
--- | this graph has no isolatedVertices
+-- | this graph has no isolatedVertices, it acts in forgetful way if isolatedVertex is added
 newtype Edges v = Edges { getEdges :: [HPair v]} deriving Show
 
--- instances --
+-- instances Vertices --
 instance  forall v e. (Eq v)=> (GraphDataSet (Vertices v e) v e []) where
   isolatedVertices g = getVertices g
   edges g  =  []
@@ -30,4 +30,29 @@ instance  forall v e. (Eq v)=> (GraphDataSet (Vertices v e) v e []) where
 instance forall v e. (DiEdgeSemantics e v) => (DiAdjacencyIndex (Vertices v e) v e []) where
    cEdgesOf g ver = []
 
--- TODO continue
+instance  forall v e. (Eq v, DiEdgeSemantics e v) => (DiGraph (Vertices v e) v e [])
+
+instance  forall v e. (Eq v, EdgeSemantics e v) => (Graph (Vertices v e) v e [])
+
+-- adding edge simply ignores edge and adds vertex
+-- TODO this should be only EdgeSemantics but it does not compile, why?
+instance  forall v e . (Eq v, DiEdgeSemantics e v) => BuildableGraphDataSet (Vertices v e) v e [] where
+
+   empty = Vertices []
+
+   g @+ v = Vertices ( v : (getVertices g) )
+   g ~+ e =
+            let HPair (v1,v2) = resolveEdge e
+            in g @+ v1 @+ v2
+
+   union g1 g2 = Vertices ((getVertices g1) ++ (getVertices g2))
+
+--------------------------------------------
+-- Adjustable Graph instance              --
+--------------------------------------------
+-- TODO - see above note about DiEdgeSemantics
+instance  forall v e . (Eq v, Eq e, DiEdgeSemantics e v) => AdjustableGraphDataSet (Vertices v e) v e [] where
+
+   g @\ f = Vertices ( filter f (getVertices g) )
+
+   filterEdges strict g f = g
