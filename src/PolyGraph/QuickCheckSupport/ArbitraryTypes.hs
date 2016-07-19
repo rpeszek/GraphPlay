8ee101d600f692965b1e58f758a11293df70f8b0
rpeszek <peszeks@gmail.com>
2016-07-18 20:42:43 -0600

    QuickCheck support

--- src/PolyGraph/QuickCheckSupport/ArbitraryTypes.hs
+++ src/PolyGraph/QuickCheckSupport/ArbitraryTypes.hs
@@ -11,22 +11,30 @@ import PolyGraph.Instances.ListGraphs
 class VertexNames a where
   vName :: Int -> a
 
-newtype UniqueUOPairs v = UniqueUOPairs { getArbitraryUOPs ::[UOPair v]}
-newtype UniqueOPairs v = UniqueOPairs { getArbitraryOPs ::[OPair v]}
-newtype IsolatedVertices v = IsolatedVertices { getArbitraryVs:: [v]}
+instance VertexNames(Int) where
+  vName = id
+
+instance VertexNames(String) where
+  vName i = "v"++show(i)
+
+newtype UniqueUOPairs v = UniqueUOPairs { getArbitraryUOPs ::[UOPair v]} deriving Show
+newtype UniqueOPairs v = UniqueOPairs { getArbitraryOPs ::[OPair v]} deriving Show
+newtype IsolatedVertices v = IsolatedVertices { getArbitraryVs:: [v]} deriving Show
 
 instance forall v. (VertexNames v) => Arbitrary(UniqueUOPairs v) where
   arbitrary = do
     v1Ids <- sublistOf [1..15]
     v2Ids <- sublistOf ([1..15] L.\\ (L.drop 1 v1Ids)) -- v1s and v2s can have at most one element in common
-    return (UniqueUOPairs $ [UOPair(vName v1Id, vName v2Id) | v1Id <- v1Ids, v2Id <- v2Ids] )
+    subset  <- sublistOf ( [UOPair(vName v1Id, vName v2Id) | v1Id <- v1Ids, v2Id <- v2Ids] )
+    return (UniqueUOPairs subset)
 
 -- 1,3,5,6   1,5,9,10
 instance forall v. (VertexNames v) => Arbitrary(UniqueOPairs v) where
   arbitrary = do
     v1Ids <- sublistOf [1..15]
     v2Ids <- sublistOf [1..15]
-    return (UniqueOPairs $ [OPair(vName v1Id, vName v2Id) | v1Id <- v1Ids, v2Id <- v2Ids] )
+    subset <- sublistOf ([OPair(vName v1Id, vName v2Id) | v1Id <- v1Ids, v2Id <- v2Ids] )
+    return $ UniqueOPairs subset
 
 instance forall v. (VertexNames v) => Arbitrary(IsolatedVertices v) where
   arbitrary = do
