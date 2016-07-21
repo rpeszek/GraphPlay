c12df25f677b77ad986ee02ab99191c01fac6b1c
rpeszek <peszeks@gmail.com>
2016-07-20 22:34:45 -0600

    namespace refactoring

--- src/PolyGraph/Buildable/Properties.hs
+++ src/PolyGraph/Buildable/Properties.hs
@@ -21,11 +21,20 @@ prop_notForgetfulG emptyG bag =
       in ((e graph == length es) && (length (isolatedVertices graph) == length isolatedVs))
 
 --
+prop_esForgetfulKeepsVs :: forall g v b e. (Eq v,
+                                   VertexNames v,
+                                   PairLike e v,
+                                   MixedBag b v e,
+                                   BuildableGraphDataSet g v e []) =>
+                                                       g ->  b  -> Bool
+prop_esForgetfulKeepsVs emptyG bag =
+      let graph = buildGraph emptyG (getMix bag) :: g
+          (es, isolatedVs, connVs) = analyze bag :: ([e], [v], [v])
+      in ((e graph == 0) && (defaultVertexCount toPair graph) == (length connVs + length isolatedVs))
+
+
 prop_esForgetfulKeepsVsG :: forall g v. (Eq v,
                                    VertexNames v,
                                    BuildableGraphDataSet g v (UOPair v) []) =>
                                                        g ->  MultiUOBag v -> Bool
-prop_esForgetfulKeepsVsG emptyG bag =
-      let graph = buildGraph emptyG (getMix bag) :: g
-          (es, isolatedVs, connVs) = analyze bag :: ([UOPair v], [v], [v])
-      in ((e graph == 0) && (defaultVertexCount toPair graph) == (length connVs + length isolatedVs))
+prop_esForgetfulKeepsVsG = prop_esForgetfulKeepsVs
