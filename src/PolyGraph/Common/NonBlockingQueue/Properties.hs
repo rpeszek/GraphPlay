157fbdcc45167dbf24308661fd325a11bd6123d4
rpeszek <peszeks@gmail.com>
2016-08-07 15:09:43 -0600

    queue

--- src/PolyGraph/Common/NonBlockingQueue/Properties.hs
+++ src/PolyGraph/Common/NonBlockingQueue/Properties.hs
@@ -4,51 +4,50 @@ module PolyGraph.Common.NonBlockingQueue.Properties (
     , dequeuesUnlessEmpty
     , QueueInstruction(..)
 ) where
-  
-import qualified PolyGraph.Common as Common
+
+import Control.Monad (liftM)
 import qualified PolyGraph.Common.NonBlockingQueue as Q
 import qualified Data.List as L
 import qualified Data.Maybe as M
 import qualified Test.QuickCheck as Property
 
-data QueueInstruction = Enqueue | Dequeue deriving (Eq, Show, Read)
+data QueueInstruction a = Enqueue a | Dequeue deriving (Eq, Show, Read)
 
-instance Property.Arbitrary QueueInstruction where
-   arbitrary = (Property.arbitrary :: Property.Gen Bool) >>= (return . (Common.fstOrSnd Enqueue Dequeue))
+instance Property.Arbitrary a => Property.Arbitrary (QueueInstruction a) where
+   arbitrary = do 
+      enqueue <- Property.arbitrary
+      if enqueue
+      then liftM Enqueue Property.arbitrary
+      else return Dequeue
 
 --
 -- Properties which make for a valid NonBlocking Queue
 --
-isFifo :: Eq a => ([a],[QueueInstruction]) -> Bool
-isFifo (source, instructions) =
-          let  results  = runQueue (source, instructions) Q.emptyQueue
+isFifo :: Eq a => [QueueInstruction a] -> Bool
+isFifo instructions =
+          let  input = map (\(Enqueue a) -> a) . L.filter (/= Dequeue) $ instructions
+               results  = runQueue instructions Q.emptyQueue
                hitResults = map M.fromJust . L.filter (M.isJust) . map fst $ results
                hitSize = L.length hitResults
-          in hitResults == L.take hitSize source
+          in hitResults == L.take hitSize input
 
-dequeuesUnlessEmpty :: ([a],[QueueInstruction]) -> Bool
-dequeuesUnlessEmpty (source, instructions) =
-          let results = runQueue (source, instructions) Q.emptyQueue
+dequeuesUnlessEmpty :: [QueueInstruction a] -> Bool
+dequeuesUnlessEmpty instructions =
+          let results = runQueue instructions Q.emptyQueue
            in all (Q.isEmpty . snd) . L.filter (M.isNothing . fst) $ results
 
 
 --
--- runQueue (sourceList, instructionList) initialQueue
+-- runQueue instructionList initialQueue
 --
--- instructionList elements:  
--- True = enqueue next element from source list, 
--- False = dequeue
---
--- Runs queue instructions against source and returns
+-- Runs queue instructions and returns
 -- Maybe results from all dequeue operations and current queue states.
--- If sourceList is exhausted, it just runs moreInst dequeue operations
 --
-runQueue :: ([a],[QueueInstruction]) -> Q.SimpleQueue a -> [(Maybe a, Q.SimpleQueue a)]
-runQueue (_,           []                  ) _  = []
-runQueue (src,         (Dequeue : moreInst)) q  =
+runQueue :: ([QueueInstruction a]) -> Q.SimpleQueue a -> [(Maybe a, Q.SimpleQueue a)]
+runQueue []                   _  = []
+runQueue (Dequeue : moreInst) q  =
                               let (x, newQ) = Q.dequeue q
-                              in (x, q) : (runQueue (src, moreInst) newQ)
-runQueue ([],          (Enqueue : moreInst))  q  = runQueue ([], moreInst) q
-runQueue ((a:moreSrc), (Enqueue : moreInst))  q  =
+                              in (x, q) : (runQueue moreInst newQ)
+runQueue (Enqueue a: moreInst) q  =
                               let newQ = a `Q.enqueue` q
-                              in runQueue (moreSrc, moreInst) newQ
+                              in runQueue moreInst newQ
--- test/Common/NonBlockingQueueSpec.hs
+++ test/Common/NonBlockingQueueSpec.hs
@@ -5,19 +5,13 @@ import Test.Hspec
 import Test.QuickCheck
 import qualified PolyGraph.Common.NonBlockingQueue.Properties as QProp
 
-isFifo :: ([Int],[QProp.QueueInstruction]) -> Bool
-isFifo = QProp.isFifo
-
-dequeuesUnlessEmpty :: ([Int],[QProp.QueueInstruction]) -> Bool
-dequeuesUnlessEmpty = QProp.dequeuesUnlessEmpty
-
 spec :: Spec
 spec = do
   describe "NonBlockingQueue" $ do
     it "isFifo" $ property $
-          isFifo 
+          (QProp.isFifo :: ([QProp.QueueInstruction Int]) -> Bool)
     it "dequeues with something unless queue is empty" $ property $
-          dequeuesUnlessEmpty
+          (QProp.dequeuesUnlessEmpty :: ([QProp.QueueInstruction Int]) -> Bool)
 
 main :: IO ()
 main = hspec spec
