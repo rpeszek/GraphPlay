7321569e3b800e6f8947d4e5a7aa3ccb5cd07b37
rpeszek <peszeks@gmail.com>
2016-08-11 20:36:40 -0600

    BFS work

--- src/FreeDSL/VTraversal.hs
+++ src/FreeDSL/VTraversal.hs
@@ -2,10 +2,17 @@ module FreeDSL.VTraversal (
   VTraverseDslCmds(..)
   , VTraversal
   , VObservation(..)
+  -- low level API
   , startAt
-  , nextVertex
+  , nextObservation
+  , currentObservation
   , put
-  , getWithDefault 
+  -- easy API
+  , getWithDefault
+  , startWith
+  , label
+  , getLabel
+  , appendLabel
 ) where
   
 --import Control.Monad
@@ -20,9 +27,10 @@ data VObservation v = Observe {
 
 data VTraverseDslCmds a v n = 
                     StartAt   v n                     |
-                    NextVertex  (VObservation v -> n) |
+                    NextVObs  (VObservation v -> n)   |
+                    CurrentVObs (VObservation v -> n) |
                     Put  (v, a) n                     |
-                    Get  (a, v) ( a -> n)
+                    Get  v ( Maybe a -> n)
                                deriving (Functor)
 
 type VTraversal a v = Free (VTraverseDslCmds a v)
@@ -30,11 +38,37 @@ type VTraversal a v = Free (VTraverseDslCmds a v)
 startAt :: forall a v .  v -> VTraversal a v ()
 startAt v  = liftF (StartAt v ())
 
-nextVertex :: forall a v . VTraversal a v (VObservation v)
-nextVertex  = liftF (NextVertex  id)
+nextObservation :: forall a v . VTraversal a v (VObservation v)
+nextObservation  = liftF (NextVObs  id)
+
+currentObservation :: forall a v . VTraversal a v (VObservation v)
+currentObservation  = liftF (CurrentVObs  id)
 
 put :: forall a v .  v -> a -> VTraversal a v ()
 put  v a = liftF (Put (v,a) ())
 
+get :: forall a v .   v -> VTraversal a v (Maybe a)
+get v = liftF (Get v id)
+
 getWithDefault :: forall a v .  a -> v -> VTraversal a v a
-getWithDefault defA v = liftF (Get (defA,v) id)
+getWithDefault defA v = get v >>= return . (maybe defA id) 
+
+startWith :: forall a v .  v -> a -> VTraversal a v ()
+startWith v a = startAt v >> put v a
+
+label :: forall a v . a -> VTraversal a v ()
+label a = currentObservation >>= 
+             (\obs -> case obs of
+                          NoMore -> return ()
+                          Observe _ v2 -> put v2 a
+             )
+
+getLabel :: forall a v . VTraversal a v (Maybe a)
+getLabel = currentObservation >>= 
+          (\obs -> case obs of
+                       NoMore -> return Nothing
+                       Observe v1 _ -> get v1
+          )
+
+appendLabel :: forall a v . Monoid a => a -> VTraversal a v (Maybe a)
+appendLabel a = getLabel >>= return . (maybe Nothing (Just . mappend a))
--- src/FreeDSL/VTraversal/BSF.hs
+++ src/FreeDSL/VTraversal/BSF.hs
@@ -23,48 +23,48 @@ import qualified Data.HashSet as HS
 runBSFState :: forall a g v e t r . (Hashable v, Eq v, AdjacencyIndex g v e t) => 
                                DSL.VTraversal a v r -> g -> HM.HashMap v a
 runBSFState program g  = 
-                        let initdata = (Nothing, [], Q.emptyQueue, HS.empty, HM.empty)
-                            (_,_,_,_,hm) = execState (interpretBSF g program) initdata
+                        let initdata = (DSL.NoMore, Nothing, [], Q.emptyQueue, HS.empty, HM.empty)
+                            (_,_,_,_,_,hm) = execState (interpretBSF g program) initdata
                         in hm
 
 runBSF :: forall a g v e t . (Hashable v, Eq v, AdjacencyIndex g v e t) => 
                                DSL.VTraversal a v a -> g -> a
-runBSF program g  = evalState (interpretBSF g program) (Nothing, [], Q.emptyQueue, HS.empty, HM.empty)
+runBSF program g  = evalState (interpretBSF g program) (DSL.NoMore, Nothing, [], Q.emptyQueue, HS.empty, HM.empty)
 
-type BSFState v a = (Maybe v, [v], Q.SimpleQueue v, HS.HashSet v, HM.HashMap v a)
+type BSFState v a = (DSL.VObservation v, Maybe v, [v], Q.SimpleQueue v, HS.HashSet v, HM.HashMap v a)
 
 interpretBSF :: forall a g v e t r . (Hashable v, Eq v, AdjacencyIndex g v e t) => 
                                g -> DSL.VTraversal a v r -> State (BSFState v a) r
 
-interpretBSF g (Free (DSL.StartAt root next)) = (put (Nothing, [], Q.enqueue root Q.emptyQueue, HS.empty, HM.empty)) >> interpretBSF g next
-interpretBSF g (Free (DSL.NextVertex vNext)) = do
+interpretBSF g (Free (DSL.StartAt root next)) = (put (DSL.NoMore, Nothing, [], Q.enqueue root Q.emptyQueue, HS.empty, HM.empty)) >> interpretBSF g next
+interpretBSF g (Free (DSL.NextVObs vNext)) = do
              s <- get
-             let (mvpair,ns) = traversalHelper g s
+             let (obs,ns) = traversalHelper g s
              put ns
-             -- maybe :: b -> (a -> b) -> Maybe a -> b
-             maybe (interpretBSF g (vNext DSL.NoMore)) 
-                   (\(v1,v2) -> interpretBSF g (vNext (DSL.Observe v1 v2)))
-                   mvpair
+             interpretBSF g (vNext obs)
+interpretBSF g (Free (DSL.CurrentVObs vNext)) = do
+             (o,_, _, _, _, _) <- get
+             interpretBSF g (vNext o)
 interpretBSF g (Free (DSL.Put va next)) = (modify (putHelper va)) >> interpretBSF g next
-interpretBSF g (Free (DSL.Get (adef, v) aNext)) = do
-             (_, _, _, _, hm) <- get
-             let a = HM.lookupDefault adef v hm
+interpretBSF g (Free (DSL.Get v aNext)) = do
+             (_, _, _, _, _, hm) <- get
+             let a = HM.lookup v hm
              interpretBSF g (aNext a)
 interpretBSF _ (Pure r) = return r
 
 putHelper :: (Hashable v, Eq v) => (v, a) -> BSFState v a -> BSFState v a
-putHelper (v0, a) (v, vs, queue, vlist, hm) = (v, vs, queue, vlist, HM.insert v0 a hm)
+putHelper (v0, a) (o, v, vs, queue, vlist, hm) = (o, v, vs, queue, vlist, HM.insert v0 a hm)
 
 traversalHelper :: forall a g v e t . (AdjacencyIndex g v e t, Hashable v, Eq v) =>
-                 g -> BSFState v a -> (Maybe (v,v), BSFState v a)
+                 g -> BSFState v a -> (DSL.VObservation v, BSFState v a)
 -- v is marked visited when is processed all elements in the queue are visited
-traversalHelper g (Nothing, _, queue, visited, hm) = 
+traversalHelper g (_, Nothing, _, queue, visited, hm) = 
       let (newV, newQ) = Q.dequeue queue
           neighborVs = HS.fromList . (neighborsOf g)
           notVisitedVs v0 = HS.toList $ HS.difference (neighborVs v0) visited
       in case newV of 
-         Nothing -> (Nothing, (Nothing, [], newQ, visited, hm))
-         Just v0 -> traversalHelper g (newV, notVisitedVs v0, newQ, visited, hm)
+         Nothing -> (DSL.NoMore, (DSL.NoMore, Nothing, [], newQ, visited, hm))
+         Just v0 -> traversalHelper g (DSL.NoMore, newV, notVisitedVs v0, newQ, visited, hm)
 
-traversalHelper g (Just _, [], queue, visited, hm) = traversalHelper g (Nothing, [], queue, visited, hm)
-traversalHelper _ (Just v0, v1:rest, queue, visited, hm) = (Just (v0, v1), (Just v0, rest, Q.enqueue v1 queue, HS.insert v1 visited, hm))
+traversalHelper g (_, Just _, [], queue, visited, hm) = traversalHelper g (DSL.NoMore, Nothing, [], queue, visited, hm)
+traversalHelper _ (_, Just v0, v1:rest, queue, visited, hm) = (DSL.Observe v0 v1, (DSL.Observe v0 v1, Just v0, rest, Q.enqueue v1 queue, HS.insert v1 visited, hm))
